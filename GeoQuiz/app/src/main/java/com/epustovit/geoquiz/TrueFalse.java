071bdafdcd70b9208196b8ff220849dffdd9b12b
shpp-epustovit <smithpev@gmail.com>
2016-05-12 15:41:14 +0300

    one activity with logging

---
+++ GeoQuiz/app/src/main/java/com/epustovit/geoquiz/TrueFalse.java
@@ -0,0 +1,31 @@
+package com.epustovit.geoquiz;
+
+/**
+ * Created by User on 11.05.2016.
+ */
+public class TrueFalse {
+    private int mQuestion;
+
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion){
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+    }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(int question) {
+        mQuestion = question;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean trueQuestion) {
+        mTrueQuestion = trueQuestion;
+    }
+}
